3bd932d29ca6b879b783ddf3f6b8617cb0b9320c
PrzemyslawStok <przemyslaw.stoklosa@gmail.com>
2025-10-17 17:25:26 +0200

    cd ..

--- src/Main.kt
+++ src/Main.kt
@@ -3,20 +3,27 @@ fun add(a: Int, b: Int): Int {
     return a + b
 }
 
+fun sub(a: Int, b: Int): Int {
+    return a - b
+}
+
 fun div(a: Int, b: Int): Int {
     return a / b
 }
 
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun brutto(a: Double): Double {
+    return 0.0
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+
+    val a: Int = 10
+    val cena = 100.0
+
+    for(i in 1..50) {
+        //println("$a+$i= ${add(a,i)}")
+        println("netto: $cena, brutto: ${brutto(cena)}")
+
     }
 
 }
