b49f5a0383155db3263bcd8867de375f591eb285
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2025-10-24 17:09:44 +0200

    cd ..

--- src/Main.kt
+++ src/Main.kt
@@ -16,7 +16,7 @@ fun brutto(number: Double, vat: Double = 0.23): Double {
     return number * (1.0 + vat)
 }
 
-fun f_brutto(){
+fun f_brutto() {
     var cena = 0.0
     val vat = 0.23
 
@@ -27,12 +27,44 @@ fun f_brutto(){
     }
 }
 
-fun x(t: Double,a: Double): Double {
-    return a*t*t/2
+fun distance(t: Double, a: Double = 9.81): Double {
+    return a * t * t / 2
 }
 
 fun main() {
 //    f_brutto()
+    val names = listOf(
+        "Słońce",
+        "Merkury",
+        "Wenus",
+        "Ziemia",
+        "Księżyc",
+        "Mars",
+        "Jowisz",
+        "Saturn",
+        "Uran",
+        "Neptun",
+        "Pluton"
+    )
 
+    val acceleration = listOf(
+        274.0,   // Słońce
+        3.7,     // Merkury
+        8.87,    // Wenus
+        9.81,    // Ziemia
+        1.62,    // Księżyc
+        3.71,    // Mars
+        24.79,   // Jowisz
+        10.44,   // Saturn
+        8.87,    // Uran
+        11.15,   // Neptun
+        0.62     // Pluton
+    )
+    val t = 2.5
+    println("droga: ${distance(t)}")
+
+    for (i in 0..<names.size) {
+        println("Ciało niebieskie: ${names[i]} posiada przyspieszenie grawitacyjne: ")
+    }
 
 }
