1bfba07142abe1a5c9d559bbfd6e5dcdbd793053
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2025-10-24 16:46:51 +0200

    cd ..

--- src/Main.kt
+++ src/Main.kt
@@ -12,18 +12,27 @@ fun div(a: Int, b: Int): Int {
     return a / b
 }
 
-fun brutto(a: Double): Double {
-    return a * (1.0 + 0.23)
+fun brutto(number: Double, vat: Double = 0.23): Double {
+    return number * (1.0 + vat)
+}
+
+fun f_brutto(){
+    var cena = 0.0
+    val vat = 0.23
+
+    for (i in 1..50) {
+        cena = Random.nextInt(1000, 5000).toDouble()
+        println("netto: $cena, brutto: ${brutto(cena, 0.1)}")
+
+    }
+}
+
+fun x(t: Double,a: Double): Double {
+    return a*t*t/2
 }
 
 fun main() {
+//    f_brutto()
 
-    var cena = 0.0
-
-    for (i in 1..50) {
-        cena = Random.nextInt(1000,5000).toDouble()
-        println("netto: $cena, brutto: ${brutto(cena)}")
-
-    }
 
 }
