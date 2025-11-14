a7f9be2c7176ba335615e6fd92f935d72c37747b
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2025-11-14 17:04:18 +0100

    cd ..

--- src/zajecia3.kt
+++ src/zajecia3.kt
@@ -8,7 +8,13 @@ open class Person(val name: String, val surname: String, val age: Int = 10, val 
     }
 }
 
-class Student(name: String, surname: String, idNumber: Int=10) : Person(name, surname) {
+class Student(name: String, surname: String, val idNumber: Int = 10) : Person(name, surname) {
+    override fun toString(): String {
+        return super.toString() + ", Id: $idNumber"
+    }
+}
+
+class Lecturer(name: String, surname: String, val l_name: String) : Person(name, surname) {
 
 }
 
@@ -16,7 +22,10 @@ fun main() {
     val personList = mutableListOf(Student("Przemysław", "Stokłosa"))
 
     for (i in 1..10) {
-        personList.add(Student("Jan_$i", "Kowalski_$i", i + 10))
+        personList.add(Student("Jan_$i", "Kowalski_$i"))
     }
     println(personList)
+
+    val ps = Lecturer("Jan", "Kowalski", "Lecturer")
+    println(ps)
 }
