ce3d07323ff38cc40aa45f40cd76c23bc7d6d765
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2025-11-28 17:39:14 +0100

    cd ..

--- src/lambda.kt
+++ src/lambda.kt
@@ -8,6 +8,10 @@ fun complex_fun(a: Int, b: Int, symbol: Char, f: (Int, Int) -> Int) {
     println("$a $symbol $b = ${f(a, b)}")
 }
 
+fun f_power(a: Int, power_no: Int, f: (Int) -> Int) {
+    println("$a^$power_no = ${f(a)}")
+}
+
 fun main() {
     val a = 10
     val b = 20
@@ -28,5 +32,20 @@ fun main() {
     complex_fun(a, b, '+', { a: Int, b: Int -> a + b })
     complex_fun(a, b, '*', { a: Int, b: Int -> a * b })
     complex_fun(a, b, '⇒', { a: Int, b: Int -> 2 * a * b })
+
+    var f3: (Double) -> (Double) = { x -> x * x }
+    f3 = { it * it }
+
+    f_power(2, 3, { it * it * it })
+    f_power(3, 5) {
+        it * it * it * it * it
+    }
+
+    val number_list = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    number_list.forEach { println(it * it) }
+    val filtered_list = number_list.filter { it % 2 == 0 }
+    println(filtered_list)
+
 
 }
