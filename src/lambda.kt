536234496c02ebe23d04f2599e1a61a51bc724b9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2025-11-28 17:06:58 +0100

    cd ..

--- src/lambda.kt
+++ src/lambda.kt
@@ -1,5 +1,32 @@
+fun add_1(a: Int, b: Int): Int {
+    return a + b
+}
 
+fun add_2(a: Int, b: Int) = a + b
 
-fun main(){
+fun complex_fun(a: Int, b: Int, symbol: Char, f: (Int, Int) -> Int) {
+    println("$a $symbol $b = ${f(a, b)}")
+}
+
+fun main() {
+    val a = 10
+    val b = 20
+    println("$a + $b = ${add_1(a, b)}")
+    println("$a + $b = ${add_2(a, b)}")
+
+    var f0: (Double) -> Int
+    var f1: (Int, Int) -> Int = { x, y -> x + y }
+
+    println("f1($a, $b) = ${f1(a, b)}")
+
+    f1 = { x: Int, y: Int -> x * y }
+
+    println("f1($a, $b) = ${f1(a, b)} ")
+
+    var f2: (Double, Double, Double) -> Double = { x, y, z -> x * y * z }
+
+    complex_fun(a, b, '+', { a: Int, b: Int -> a + b })
+    complex_fun(a, b, '*', { a: Int, b: Int -> a * b })
+    complex_fun(a, b, '⇒', { a: Int, b: Int -> 2 * a * b })
 
 }
